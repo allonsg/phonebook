1580ac52dd8a3a076d498b943d60ed3f145b5abc
allonsg <109518407+allonsg@users.noreply.github.com>
2023-02-05 12:14:11 +0200

    added redux-persist

---
+++ src/common/token.js
@@ -0,0 +1,7 @@
+const localObj = JSON.parse(localStorage.getItem('persist:user'));
+const token =
+  localObj && localObj.token.length > 10
+    ? localObj.token.slice(1, localObj.token.length - 1)
+    : '';
+
+export default token;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { contactsSlice } from './contactsSlice';
 import { userSlice } from './userSlice';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const userPersistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     contacts: contactsSlice.reducer,
-    user: userSlice.reducer,
+    user: persistReducer(userPersistConfig, userSlice.reducer),
   },
+  middleware,
 });
+
+export const persistor = persistStore(store);
